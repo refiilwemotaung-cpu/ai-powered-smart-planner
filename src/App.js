9c34da9858ab7704f8651a48af8b9d465a092918
Mafifiza <refilwemotaung2025@outlook.com>
2025-10-03 18:39:42 +0200

    Set up App.js with context providers and component structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+import React from "react";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { TaskProvider } from "./contexts/TaskContext";
+import Header from "./components/Header";
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import AIAssistant from "./components/AIAssistant";
+import WatchView from "./components/WatchView";
 import "./styles/App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ThemeProvider>
+      <TaskProvider>
+        <div className="app">
+          <WatchView />
+          <div className="desktop-view">
+            <Header />
+            <main className="main-content">
+              <div className="container">
+                <AIAssistant />
+                <TaskForm />
+                <TaskList />
+              </div>
+            </main>
+          </div>
+        </div>
+      </TaskProvider>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useTheme } from "../contexts/ThemeContext";
-import "..styles/Header.css";
+import "../styles/Header.css";
 
 const Header = () => {
   const { isDarkMode, toggleTheme } = useTheme();
