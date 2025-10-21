ca9f761c24164814a34ed0570be4b4178d9863dd
Mafifiza <refilwemotaung2025@outlook.com>
2025-10-21 20:06:18 +0200

    Update App.js with Theme Provider and Header

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
 import React from "react";
+import { ThemeProvider } from "./contexts/ThemeContexts";
 import { TaskProvider } from "./contexts/TaskContext";
+import Header from "./components/Header";
 import TaskForm from "./components/TaskForm";
-import TaskList from "./components/TaskList";
-import AIAssistant from "./components/AIAssistant";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/App.css";
 
 function App() {
   return (
-    <TaskProvider>
-      <div className="app">
-        <header className="app-header">
-          <h1>🧠 AI SmartPlanner</h1>
-          <p>Your intelligent task manager</p>
-        </header>
-        <main className="main-content">
-          <div className="container">
-            <AIAssistant />
-            <TaskForm />
-            <TaskList />
-          </div>
-        </main>
-      </div>
-    </TaskProvider>
+    <ThemeProvider>
+      <TaskProvider>
+        <div className="app">
+          <Header />
+          <main className="main-content">
+            <div className="container">
+              <TaskForm />
+            </div>
+          </main>
+        </div>
+      </TaskProvider>
+    </ThemeProvider>
   );
 }
 
