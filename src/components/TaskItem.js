a69dfec708b9fbe195ee222cda7949bf30505668
Mafifiza <refilwemotaung2025@outlook.com>
2025-10-21 19:13:19 +0200

    Create TaskItem CSS

--- src/components/TaskItem.js
+++ src/components/TaskItem.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useTasks } from "../contexts/TaskContext";
-//import "./TaskItem.css";
+import "./TaskItem.css";
 
 const TaskItem = ({ task }) => {
   const { updateTask, deleteTask, toggleTaskCompletion } = useTasks();
