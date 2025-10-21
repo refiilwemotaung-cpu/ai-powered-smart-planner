fae2fc56d7a73371205bd14e94e581e776ff0e5d
Mafifiza <refilwemotaung2025@outlook.com>
2025-10-21 22:47:18 +0200

    Update App.js to conditionally show WatchView

--- src/App.js
+++ src/App.js
@@ -3,20 +3,33 @@ import { ThemeProvider } from "./contexts/ThemeContexts";
 import { TaskProvider } from "./contexts/TaskContext";
 import Header from "./components/Header";
 import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import AIAssistant from "./components/AIAssistant";
+import WatchView from "./components/WatchView";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/App.css";
 
 function App() {
+  const isWatchView = window.innerWidth <= 240;
+
   return (
     <ThemeProvider>
       <TaskProvider>
         <div className="app">
-          <Header />
-          <main className="main-content">
-            <div className="container">
-              <TaskForm />
-            </div>
-          </main>
+          {isWatchView ? (
+            <WatchView />
+          ) : (
+            <>
+              <Header />
+              <main className="main-content">
+                <div className="container">
+                  <AIAssistant />
+                  <TaskForm />
+                  <TaskList />
+                </div>
+              </main>
+            </>
+          )}
         </div>
       </TaskProvider>
     </ThemeProvider>
